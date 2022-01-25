cfaad99faf27eb76e022ec4db7bb21addfa56657
nablam <lamribenn@gmail.com>
2022-01-25 11:32:25 -0500

    all set with git ignores and package managements, next try fbx explorere

---
+++ HIDGamePad/HIDGamePad/HIDGamePad.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		HIDGamePad.ino
+ Created:	1/25/2022 11:21:51 AM
+ Author:	Nabz
+*/
+
+// the setup function runs once when you press reset or power the board 
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
